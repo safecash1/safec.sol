7fc46bc6fbbb3d9583f29155fa66026994f843b6
safecash1 <83125397+safecash1@users.noreply.github.com>
2021-04-27 23:50:46 +0800

    Update safec.sol

--- safec.sol
+++ safec.sol
@@ -1,21 +1,3 @@
-/**
- *Submitted for verification at BscScan.com on 2021-03-01
-*/
-
-/**
- *Submitted for verification at BscScan.com on 2021-03-01
-*/
-
-/**
-
-   #SafeCash features:
-   3% fee auto add to the liquidity pool to locked forever when selling
-   2% fee auto distribute to all holders
-   I created a black hole so #Bee token will deflate itself in supply with every transaction
-   50% Supply is burned at start.
-   
-
- */
 pragma solidity ^0.6.12;
 // SPDX-License-Identifier: Unlicensed
 interface IERC20 {
@@ -696,7 +678,7 @@ interface IUniswapV2Router02 is IUniswapV2Router01 {
 }
 
 
-contract SafeMoon is Context, IERC20, Ownable {
+contract SafeCash is Context, IERC20, Ownable {
     using SafeMath for uint256;
     using Address for address;
 
